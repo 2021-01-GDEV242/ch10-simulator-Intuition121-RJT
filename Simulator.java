7c162809ad26525fe93285f0f4005b46a73f0386
Intuition121-RJT <77683482+Intuition121-RJT@users.noreply.github.com>
2021-04-12 14:37:37 -0400

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -21,7 +21,9 @@ public class Simulator
     // The probability that a fox will be created in any given grid position.
     private static final double FOX_CREATION_PROBABILITY = 0.02;
     // The probability that a rabbit will be created in any given grid position.
-    private static final double RABBIT_CREATION_PROBABILITY = 0.08;    
+    private static final double RABBIT_CREATION_PROBABILITY = 0.08;
+    // The probability that a psycho will be created in any given grid position.
+    private static final double PSYCHO_CREATION_PROBABILITY = 0.001;
 
     // List of animals in the field.
     private List<Animal> animals;
@@ -61,6 +63,7 @@ public class Simulator
         view = new SimulatorView(depth, width);
         view.setColor(Rabbit.class, Color.ORANGE);
         view.setColor(Fox.class, Color.BLUE);
+        view.setColor(Psychopath.class, Color.RED);
         
         // Setup a valid starting point.
         reset();
@@ -145,6 +148,11 @@ public class Simulator
                     Location location = new Location(row, col);
                     Rabbit rabbit = new Rabbit(true, field, location);
                     animals.add(rabbit);
+                }
+                else if(rand.nextDouble() <= PSYCHO_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Psychopath psycho = new Psychopath(true, field, location);
+                    animals.add(psycho);
                 }
                 // else leave the location empty.
             }
